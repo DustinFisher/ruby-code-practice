ece181713f481cc62316e7bf322e8627643d9b1c
Dustin Fisher <dustin@dustinfisher.com>
2012-07-03 07:41:42 -0400

    solution to problem 4

--- project-euler/4.rb
+++ project-euler/4.rb
@@ -3,3 +3,18 @@
 # made from the product of two 2-digit numbers is 9009 = 91 99. Find the 
 # largest palindrome made from the product of two 3-digit numbers.
 
+def largest_palindrome(digits)
+	numbers = []
+	x = ("9"*digits).to_i
+	y = ("9"*digits).to_i
+	limit = (("9"*(digits-1)).to_i)
+	while x > limit
+		y.downto(limit+1) do |i|
+			x*i == (x*i).to_s.reverse.to_i ? numbers << x*i : ""
+		end
+		x -= 1
+	end
+	return numbers.sort.last
+end
+
+puts largest_palindrome(3)
